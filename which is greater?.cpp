f2b7042c4f00a885e079bfba66e93d453bf61732
am0654 <94629609+am0654@users.noreply.github.com>
2023-05-03 13:59:21 +0530

    Create which is greater?.cpp

---
+++ which is greater?.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main{
+int a,b;
+cin>>"enter value of a: ">>a;
+cin>>"enter value of b: ">>b;
+if(a>b){
+  cout<<"a is greater";
+ }
+else{
+  cout<<"b is greater";
+}
+  return 0;
+}
+
+Input:
+enter value of a: 25
+enter value of b: 20
+  
+Output:
+a is greater
