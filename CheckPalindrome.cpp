31d26c82607d059a696b19821a1b99daee5153b5
am0654 <94629609+am0654@users.noreply.github.com>
2023-05-06 23:03:26 +0530

    Create CheckPalindrome.cpp

---
+++ CheckPalindrome.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num, rev=0, rem, temp;
+    cout<<"Enter the Number/word : ";
+    cin>>num;
+    temp = num;
+    while(temp>0)
+    {
+        rem = temp%10;
+        rev = (rev*10)+rem;
+        temp = temp/10;
+    }
+    if(rev==num)
+        cout<<"\nIt is a Palindrome";
+    else
+        cout<<"\nIt is not a Palindrome";
+    cout<<endl;
+    return 0;
+}
+
+Input:
+Enter the Number/word : 2052502
+  
+Output:
+It is a Palindrome Number
