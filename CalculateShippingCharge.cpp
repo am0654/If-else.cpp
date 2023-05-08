58f2c4373018070b7b5f96c37921513b6412968f
am0654 <94629609+am0654@users.noreply.github.com>
2023-05-08 23:26:17 +0530

    Create CalculateShippingCharge.cpp

---
+++ CalculateShippingCharge.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+
+using namespace std;
+int main()
+{
+   float weight, basecharge=28.50, perkgcharge=4, temp, extracharge, charge;
+   cout<<"Enter Parcel's Weight (in Kg): ";
+   cin>>weight;
+   if(weight<=2)
+      cout<<"\nShipping Charge: "<<basecharge;
+   else
+   {
+      temp = weight-2;
+      extracharge = temp * perkgcharge;
+      charge = extracharge + basecharge;
+      cout<<"\nShipping Charge: "<<charge;
+   }
+   cout<<endl;
+   return 0;
+}
+
+
+Input:
+Enter Parcel's Weight (in Kg): 200
+  
+Output:
+Shipping Charge: 820.5
